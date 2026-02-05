ca4a3947e94ecd73953bd2c4aea9f2e7b93f0b8f
“kelvinkn17” <kelvin.kn17@gmail.com>
2026-02-05 23:17:44 +0700

    chore: cleanup

--- explorations/nitrolite-local/sdk/src/abis/custody/index.ts
+++
@@ -1,8 +0,0 @@
-export { custodyAbi as CustodyAbi } from '../generated';
-
-export const ChannelOpenedEvent = 'Opened';
-export const ChannelClosedEvent = 'ChannelClosed';
-export const ChannelChallengedEvent = 'Challenged';
-export const ChannelCheckpointedEvent = 'Checkpointed';
-export const ChannelCreatedEvent = 'Created';
-export const ChannelJoinedEvent = 'Joined';
--- explorations/nitrolite-local/sdk/src/rpc/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './nitrolite';
-export * from './utils';
-export * from './types';
-export * from './api';
-export * from './parse';
-export * from './parse';
-export * from './parse/parse';
