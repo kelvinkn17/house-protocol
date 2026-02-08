ea332ea2292a0641826a19623c7579ebaf1ef4d8
“kelvinkn17” <72134234+kelvinkn17@users.noreply.github.com>
2026-02-08 21:21:19 +0700

    fix vercel

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,8 +4,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-// nitro-nightly has internal import bug, disabled for now
-// import { nitro } from 'nitro/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: {
@@ -21,7 +20,7 @@ const config = defineConfig({
   },
   plugins: [
     devtools(),
-    // nitro(),
+    nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
